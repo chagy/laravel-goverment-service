8ee93931d17dadc1d1335da3af5d7d8f59532ad7
chagy <chagy_x_@hotmail.com>
2022-07-10 19:31:20 +0700

    3-6-Footer

---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+        Chagy Code
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-{{ date('Y') }} <a href="https://chagyza.com">chagyza.com</a></strong> All rights reserved.
+</footer>
