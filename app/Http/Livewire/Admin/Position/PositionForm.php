88ac067645a578e597f90b7bbbeec332dc4f6313
chagy <chagy_x_@hotmail.com>
2022-09-16 20:43:47 +0700

    4-5 Position Form Create

--- app/Http/Livewire/Admin/Position/PositionForm.php
+++ app/Http/Livewire/Admin/Position/PositionForm.php
@@ -3,9 +3,25 @@
 namespace App\Http\Livewire\Admin\Position;
 
 use Livewire\Component;
+use App\Models\Position;
 
 class PositionForm extends Component
 {
+    public $idKey = 0;
+    public $posi_name;
+    public $posi_desc;
+
+    public function save()
+    {
+        $position = Position::updateOrCreate(
+            ['id' => $this->idKey],
+            [
+                'posi_name' => $this->posi_name,
+                'posi_desc' => $this->posi_desc,
+            ]
+        );
+    }
+
     public function render()
     {
         return view('livewire.admin.position.position-form');
--- resources/views/livewire/admin/position/position-form.blade.php
+++ resources/views/livewire/admin/position/position-form.blade.php
@@ -1,18 +1,38 @@
-<div class="modal fade" id="modal-form">
+<div>
+<form wire:submit.prevent="save">
+<div class="modal fade" id="modal-form" wire:ignore.selt>
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
-          <h4 class="modal-title">Large Modal</h4>
+          <h4 class="modal-title">ข้อมูลตำแหน่ง</h4>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-          <p>One fine body&hellip;</p>
+            <div class="form-group">
+                <label for="posi_name">ชื่อตำแหน่ง</label>
+                <input 
+                    type="text" 
+                    id="posi_name"
+                    name="posi_name"
+                    placeholder="กรอก ชื่อตำแหน่ง"
+                    class="form-control" 
+                    wire:model="posi_name"/>
+            </div>
+            <div class="form-group">
+                <label for="posi_desc">รายละเอียด</label>
+                <textarea 
+                    id="posi_desc"
+                    name="posi_desc"
+                    placeholder="กรอก รายละเอียด"
+                    class="form-control" 
+                    wire:model="posi_desc"></textarea>
+            </div>
         </div>
         <div class="modal-footer justify-content-between">
           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="submit" class="btn btn-primary">Save</button>
         </div>
       </div>
       <!-- /.modal-content -->
@@ -20,3 +40,5 @@
     <!-- /.modal-dialog -->
 </div>
 <!-- /.modal -->
+</form>
+</div>
